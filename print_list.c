420d767cb31957dcbe47a1d844c1a6b222b13127
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-20 17:44:17 -0700

    Update print_list.c

--- print_list.c
+++ print_list.c
@@ -1,5 +1,14 @@
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
-#include "sort.h"
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
  * print_list - Prints a list of integers
@@ -8,16 +17,16 @@
  */
 void print_list(const listint_t *list)
 {
-	int i;
+    int j;
 
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
+    j = 0;
+    while (list)
+    {
+        if (j > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++j;
+        list = list->next;
+    }
+    printf("\n");
 }
