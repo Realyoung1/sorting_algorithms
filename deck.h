f08f343a98891ecc6f9b33e3b338ae656c9912e6
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-20 17:58:09 -0700

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,6 +1,16 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
 
 /**
