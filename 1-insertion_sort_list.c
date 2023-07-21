23d1ba7cbf030f9207672deeb57b6937bb15762d
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-21 12:48:45 -0700

    Create 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked listss
+ * @h: A pointer to the head of the doubly-linked listss
+ * @n1: A pointer to the first node to swapss
+ * @n2: The second node to swapss
+ */
+
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integerss
+ *                       using the insertion sort algorithmss
+ * @list: A pointer to the head of a doubly-linked list of integersss
+ *
+ * Description: Prints the list after each swapss
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	{
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
