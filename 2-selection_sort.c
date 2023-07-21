9193652158b08c192901b8caa3285b959231f3bf
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-21 12:53:11 -0700

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+
+/**
+ * swap_ints - Swap two integers in an arrayss
+ * @a: The first integer to swapss
+ * @b: The second integer to swapss
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending orderss
+ *                  using the selection sort algorithmss
+ * @array: An array of integersss
+ * @size: The size of the arrayss
+ *
+ * Description: Prints the array after each swapss
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t j, k;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		min = array + j;
+		for (k = j + 1; k < size; k++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + j) != min)
+		{
+			swap_ints(array + j, min);
+			print_array(array, size);
+		}
+	}
+}
